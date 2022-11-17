c5d8ea31601aa62d7d21e5fc7da2f2bab569ae5f
navuyi <figlusrafal@gmail.com>
2022-11-17 20:03:59 +0100

    QualityDecreaser class

--- extension/src/pages/Content/QualityDecreaser.ts
+++ extension/src/pages/Content/QualityDecreaser.ts
@@ -1,13 +1,52 @@
 import { CustomLogger } from "../../utils/classes/CustomLogger";
+import { NetflixBitrateMenu } from "../../utils/classes/NetflixBitrateMenu";
+import { wait_for_video_to_load } from "../../utils/wait_for_video_to_load";
 
-export class QualityScenarioManager {
+export class QualityDecreaser {
    
     private logger : CustomLogger
-  
+    private bitrate_index : number
+    private interval : ReturnType<typeof setInterval> | undefined
+
     constructor(){
         this.logger = new CustomLogger("[QualityScenarioManager]")
+        this.bitrate_index = 0
     }
 
+
+    public init = async () : Promise<void> => {
+        await wait_for_video_to_load()
+
+        await this.set_init_bitrate()
+        this.start_bitrate_changes()
+    }
+
+    private set_init_bitrate = async () : Promise<void> => {
+        const available_bitrates = await NetflixBitrateMenu.get_available_bitrates()
+        this.bitrate_index = available_bitrates.length-1
+
+        await this.set_bitrate()
+    }
+
+    private start_bitrate_changes = () : void => {
+        this.interval = setInterval(async () => {
+            await this.set_bitrate()
+        }, 60000)
+    }
+
+   
+    private set_bitrate = async () : Promise<void> => {
+        const bitrates = await NetflixBitrateMenu.get_available_bitrates()
+        const next_bitrate = bitrates[this.bitrate_index]
+        
+        this.decrement_bitrate_index()
+
+        await NetflixBitrateMenu.set_bitrate(next_bitrate)
+    }
+
+    private decrement_bitrate_index = (): void => {
+        this.bitrate_index > 0 ? this.bitrate_index -= 1 : this.bitrate_index = 0
+    }
 }
 
-export default QualityScenarioManager
+export default QualityDecreaser
--- extension/src/pages/Content/index.ts
+++ extension/src/pages/Content/index.ts
@@ -3,9 +3,13 @@ import { QualityEnhancer } from "./QualityEnhancer"
 import { DebugMenuAnalyzer } from "./DebugMenuAnalyzer"
 import { NetflixBitrateMenu } from "../../utils/classes/NetflixBitrateMenu"
 import { NetflixDebugMenu } from "../../utils/classes/NetflixDebugMenu"
+import QualityDecreaser from "./QualityDecreaser"
 
 
 const init = async () : Promise<void> => {
+
+    
+    inject_netflix_controls_script()
 
     //const customPlayer = new CustomPlayer()
     //await customPlayer.init()
@@ -13,16 +17,17 @@ const init = async () : Promise<void> => {
     const qualityEnhancer = new QualityEnhancer()
     await qualityEnhancer.init()
 
-    const element : HTMLTextAreaElement = await NetflixDebugMenu.get_html_element()
-    console.log(element)
+    const qualityDecreaser = new QualityDecreaser()
+    await qualityDecreaser.init()
+}
 
 
+const inject_netflix_controls_script = () => {
     const s = document.createElement("script")
     s.src = chrome.runtime.getURL("netflixControls.bundle.js");
     
     (document.head || document.documentElement).appendChild(s);
     s.remove()
-
 }
 
 init()
