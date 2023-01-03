691333c7dc1dce01d5796dc504a55acf9116d8fd
navuyi <figlusrafal@gmail.com>
2023-01-03 12:26:14 +0100

    port 5001 instead of 5000 causing problems

--- extension/src/utils/http_requests/config.js
+++ extension/src/utils/http_requests/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://127.0.0.1:5000"
+export const BASE_URL = "http://127.0.0.1:5001"
 export const backend_urls = {
     experiment: `${BASE_URL}/experiment/`,
     video: `${BASE_URL}/video/`,
