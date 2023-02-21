3ecbb0dd930ff319bfac49a523e105f85fe34922
navuyi <figlusrafal@gmail.com>
2023-02-21 11:07:23 +0100

    cleaning

--- backend/data_analysis/generate_csv.py
+++ backend/data_analysis/generate_csv.py
@@ -29,10 +29,10 @@ def main():
 
     for video in videos:
         # Get video's experiment data
-        cursor.execute(f"SELECT subject_id FROM experiment WHERE id={video['experiment_id']}")
+        cursor.execute(f"SELECT id FROM experiment WHERE id={video['experiment_id']}")
         exp_data = cursor.fetchall()[0]
 
-        csv_filename = f"{exp_data['subject_id']}_videoID_{video['id']}.csv"
+        csv_filename = f"123123123_videoID_{video['id']}.csv"
 
         # Get video playback data
         cursor.execute(f"SELECT * FROM playback_data WHERE video_id={video['id']}")
